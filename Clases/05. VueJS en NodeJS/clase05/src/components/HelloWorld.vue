547d791b2153060d3153bc7519652f7804e60a9c
Suru <nachosuruguay@gmail.com>
2022-11-28 22:32:11 -0300

    Clase05 - se crea proyecto con vue cli y nodejs

---
+++ Clases/05. VueJS en NodeJS/clase05/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1 >Proyecto clase 05</h1>
+    <h3 class="text-primary">{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
