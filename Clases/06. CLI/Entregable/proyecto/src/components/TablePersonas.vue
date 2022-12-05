4cf92e5ab5ed1789f0006623c71885197c766f5f
Suru <nachosuruguay@gmail.com>
2022-12-05 20:32:26 -0300

    Clase 06 - entregable: se crea componente TablePersonas.vue

---
+++ Clases/06. CLI/Entregable/proyecto/src/components/TablePersonas.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <section>
+        <table class="table" :class="tipo"  v-bind:style="{color: fontcolor}">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Peso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(alumno, i) of alumnos" :key="i" >
+            <td>{{alumno.nombre}}</td>
+            <td>{{alumno.edad}}</td>
+            <td>{{alumno.peso}}</td>
+          </tr>
+        </tbody>
+      </table>  
+  </section>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'TablePersonas',
+    props: {
+      alumnos: Array,
+      tipo: String,
+      fontcolor : String
+    },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped >
+</style>
