e34b346f21d7e947ae69055a3a0a396e8a0d4f7e
Suru <nachosuruguay@gmail.com>
2022-12-21 00:18:59 -0300

    Proyecto final: se crea vista y componentes de funciones/peliculas

---
+++ Clases/ProyectoFinal/proyecto/src/views/AltaFuncionView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <form-alta-funcion></form-alta-funcion>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FormAltaFuncion from '../components/FormAltaFuncion.vue'
+
+export default {
+name: 'HomeView',
+components: {
+    FormAltaFuncion,
+}
+}
+</script>
