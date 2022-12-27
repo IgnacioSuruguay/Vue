f46e86f43a5a822299c2d7c954cea1620edec227
Suru <nachosuruguay@gmail.com>
2022-12-26 21:11:41 -0300

    Proyecto final: App.vue Se modifica router

--- Clases/ProyectoFinal/proyecto/src/App.vue
+++ Clases/ProyectoFinal/proyecto/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/peliculas">Peliculas</router-link> |
+      <router-link to="/carrito">Carrito</router-link> |
+      <router-link to="/peliculas">ABM Peliculas</router-link> |
     </nav>
     <router-view/>
   </div>
